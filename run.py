48651fc7df99f9cff48e756128cf54c3f7d0e635
xu <58101142+xu737@users.noreply.github.com>
2024-02-09 13:50:58 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
 import argparse
 import os
 import torch
-# from exp.exp_long_term_forecasting import Exp_Long_Term_Forecast
-# from exp.exp_imputation import Exp_Imputation
-# from exp.exp_short_term_forecasting import Exp_Short_Term_Forecast
 from exp.exp_anomaly_detection import Exp_Anomaly_Detection
 from data_provider.shared_construct import *
-
-# from exp.exp_classification import Exp_Classification
 import random
 import numpy as np
 from data_provider.data_factory import data_provider
